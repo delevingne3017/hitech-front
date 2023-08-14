d9a7eed80e0b62b28840ef858413c8985a75944e
20B1NUM2012 BATSUKH <20B1NUM2012@stud.num.edu.mn>
2023-08-14 10:25:59 +0800

    digital ocean connected

--- next.config.js
+++ next.config.js
@@ -1,18 +1,17 @@
 /** @type {import('next').NextConfig} */
-const backEndUrl = "http://localhost:8000";
+const backEndUrl = "http://167.99.70.250:8000";
 // const uploadUrl = process.env.NEXT_PUBLIC_UPL
 
 module.exports = {
-    rewrites: async () => {
-        return [
-            {
-                source: "/api/:path*",
-                destination: `${backEndUrl}/api/:path*`
-            },
-        ]
-    },
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-}
-
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${backEndUrl}/api/:path*`,
+      },
+    ];
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
